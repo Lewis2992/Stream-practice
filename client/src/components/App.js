1a3459fa9d5ba37cdb014c64534f163c6ef75a62
Lewis2992 <lewisho2992@gmail.com>
2022-08-19 10:12:36 +0100

    implemented routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import StreamCreate from './stream/StreamCreate';
+import StreamDelete from './stream/StreamDelete';
+import StreamEdit from './stream/StreamEdit';
+import StreamList from './stream/StreamList';
+import StreamShow from './stream/StreamShow';
 
 const App = () => {
 	return (
-		<div>This is app</div>
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<StreamList/>}/>
+					<Route path="/streams/new" element={<StreamCreate/>}/>
+					<Route path="/streams/edit" element={<StreamEdit/>}/>
+					<Route path="/streams/delete" element={<StreamDelete/>}/>
+					<Route path="/streams/show" element={<StreamShow/>}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 };
 
